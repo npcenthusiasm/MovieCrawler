facfcffd9ad7502172aa79f61dca1af000dcc0ce
CHING YUAN HSU <npcenthusiasm@outlook.com>
2019-06-23 22:07:26 +0800

    0623 ajax-option

---
+++ MovieCrawler/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieCrawler.Models;
+namespace MovieCrawler.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieCrawlerEntities db = new MovieCrawlerEntities();
+
+        public ActionResult Index()
+        {
+            var query = from o in db.MovieLists
+                        select o;
+            return View(query.ToList());
+        }
+        //[HttpPost]
+        //public ActionResult Index(int MovieID)
+        //{
+        //    var query = from o in db.MovieInfoes
+        //                where o.MovieID==MovieID
+        //                select o;
+        //    return View(query.ToList());
+        //}
+
+        public ActionResult items(int MovieID)
+        {
+            ViewBag.item = MovieID;
+            return View();
+        }
+
+        public JsonResult MovieInfoData()
+        {
+            var query = from o in db.MovieInfoes
+                        select o;
+            var movieList = query.ToList();
+            return new JsonResult { Data = movieList, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
